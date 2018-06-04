004036d75fd7c98addcff40e372b77513234d5a4
bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-06-04 21:39:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.gif';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <img src="logo.gif" alt="logo" />
+        <h1>
+          Hello, World!
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default App;
