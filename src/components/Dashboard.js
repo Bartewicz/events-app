aa60ab1479d211abccef762f74dd1c16ae851266
bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-06-21 13:08:12 +0200

    Display simple events details fetched from database

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,26 +1,56 @@
 import React from 'react'
-// Firebase
-import { database } from '../firebase'
+// React-Redux
+import { connect } from 'react-redux'
+// Moment
+import Moment from 'moment'
 // UI
 import PaperRefined from '../ui/PaperRefined'
 
-class Dashboard extends React.Component {
-  render() {
-    return (
-      <main>
-        <div className={'paper'}>
-          <h1 className={'text-center no-margins'}>
-            Dashboard
+const Dashboard = (props) => (
+  <main>
+    <div className={'paper'}>
+      <h1 className={'text-center no-margins'}>
+        Dashboard
           </h1>
-        </div>
-        <PaperRefined>
-          <div>
-            events
-          </div>
-        </PaperRefined>
-      </main>
-    )
-  }
-}
+    </div>
+    <PaperRefined>
+      <div>
+        {
+          props.events.length ?
+            props.events.map((event, index) =>
+              <div key={index}>
+                <div className={'flex-space-between'}>
+                  <h3 className={'section-title display-inline no-margins'}>
+                    {event.header}
+                  </h3>
+                  <h3 className={'display-inline text-right no-margins text-gray'}>
+                    created at: {JSON.stringify(Moment(event.createdAt).format('dddd, Do MMM YYYY,'))}
+                    <br />
+                    {JSON.stringify(Moment(event.createdAt).format('h:mm:ss'))}
+                  </h3>
+                </div>
+                <p>{event.description}</p>
+                {
+                  index !== props.events.length - 1 ?
+                    <hr />
+                    :
+                    null
+                }
+              </div>
+            )
+            :
+            'Loading...'
 
-export default Dashboard
+        }
+      </div>
+    </PaperRefined>
+  </main>
+)
+
+export default connect(
+  state => ({
+    events: state.events.events
+  }),
+  dispatch => ({
+  })
+)(Dashboard)
